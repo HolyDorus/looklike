482fd4e2988e0d5bf278e1cfcb64904cf568b395
HolyDorus <marichevdenysmail@gmail.com>
2021-05-19 19:47:00 +0300

    Refactoring database pool

--- src/backend/looklike/database.py
+++ src/backend/looklike/database.py
@@ -7,21 +7,32 @@ from psycopg2.extras import RealDictCursor
 from looklike.configs import config
 
 
-url = urlparse(config.POSTGRESQL_URL)
+_connection_pool = None
 
-connection_pool = ThreadedConnectionPool(
-    minconn=1,
-    maxconn=50,
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
+
+def get_db_connection_pool():
+    global _connection_pool
+
+    if _connection_pool:
+        return _connection_pool
+
+    url = urlparse(config.POSTGRESQL_URL)
+    _connection_pool = ThreadedConnectionPool(
+        minconn=1,
+        maxconn=20,
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+
+    return _connection_pool
 
 
 @contextmanager
 def get_db_connection():
+    connection_pool = get_db_connection_pool()
     try:
         connection = connection_pool.getconn()
         yield connection
